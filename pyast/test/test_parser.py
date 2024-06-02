4fd18ee952a9ef9e1c6a96bafe643a40a60c0dc8
Mike <mikeresnik@gmail.com>
2024-06-02 12:53:44 -0400

    Adds test cases.

--- pyast/test/test_parser.py
+++ pyast/test/test_parser.py
@@ -21,6 +21,18 @@ class TestParser(unittest.TestCase):
         operation = parse("123 + abc")
         print("test_parser_addition", str(operation))
 
+    def test_parser_multiplication(self):
+        operation = parse("a * b * c + abc")
+        print("test_parser_multiplication", str(operation))
+
+    def test_parser_functions_of_functions(self):
+        operation = parse("sin(sin(x))")
+        print("test_parser_functions_of_functions", str(operation))
+
+    def test_parentheses(self):
+        operation = parse("(123 + 2i + (2x))")
+        print("test_parentheses", str(operation))
+
 
 if __name__ == '__main__':
     unittest.main()
